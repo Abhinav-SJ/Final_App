2304da7c746d5279cb086601db992c5c3e51ed2d
Abhinav-SJ <73390621+Abhinav-SJ@users.noreply.github.com>
2022-06-02 23:16:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,23 @@ crop_recommendation_model = pickle.load(open(crop_recommendation_model_path, 'rb
 
 app = Flask(__name__)
 
+def weather_fetch(city_name):
+    api_key = config.weather_api_key
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+
+    if x["cod"] != "404":
+        y = x["main"]
+
+        temperature = round((y["temp"] - 273.15), 2)
+        humidity = y["humidity"]
+        return temperature, humidity
+    else:
+        return None
+
 @ app.route('/crop_recommend', methods=['POST'])
 def crop_recommend():
     if request.method == 'POST':
@@ -29,3 +46,4 @@ def crop_recommend():
             return jsonify({'label':str(final_prediction)})
         else:
             return jsonify({'label':'Data not available'})
+
